10d891c7a09a1bcc349f903c09391df42a152fd9
4ks <107892265+4ks-devops@users.noreply.github.com>
2023-01-30 17:07:09 -0600

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>CSN Cooperativa Financiera</title>
+		<link href="./bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link href="./css/inicio.css" rel="stylesheet">
+	</head>
+	<body >
+    <div class="login">
+    	<div class="formLogin pt-5 mt-5 pb-5 mb-5">
+  	  		<div class="col-md-12 col-sm-12 text-center pt-4 mt-4">
+                <h6 class="lblNombre">Bienvenido</h3>
+                <div class="pt-3 mt-3">
+                    <img src="img/user.png" alt="" class="img-responsive imgUsuario">
+                </div>
+  	  		</div>
+    	</div>
+    </div>
+    <!-- jQuery -->
+
+    <script src="./js/jquery-3.6.1.min.js"></script>
+    <script src="./js/login.js"></script>
+    <!-- Bootstrap 5 -->
+    <script src="./bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@microsoft/microsoft-graph-client/lib/graph-js-sdk.js"></script>
+    <!-- polyfilling promise -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/es6-promise/dist/es6-promise.auto.min.js"></script>
+    <!-- polyfilling fetch -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/whatwg-fetch/dist/fetch.umd.min.js"></script>
+    <!-- depending on your browser you might wanna include babel polyfill -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@babel/polyfill@7.4.4/dist/polyfill.min.js"></script>
+  </body>
+</html>
